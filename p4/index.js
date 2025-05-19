c43846696cb846580122d77c67f84ce5af48e67d
Sravanthi931 <ksravs917@gmail.com>
2025-05-19 20:07:20 +0530

    up

--- p4/index.js
+++ p4/index.js
@@ -1,7 +1,7 @@
-const users = {
-  "12345678": { pin: "1111", name: "Alice" },
-  "87654321": { pin: "2222", name: "Bob" }
-};
+const customers = [
+  { cart: "1234567890", pin: "1234", name: "Sravanthi", balance: 0 },
+  { cart: "1234567891", pin: "2345", name: "Cathy", balance: 0 },
+];
 
 function submit() {
   const card = document.getElementById("cardNumber").value;
